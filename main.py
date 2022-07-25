3a0aa4480db5562116df138afa7f370c73ef0532
asptu <84566762+asptu@users.noreply.github.com>
2022-07-26 00:27:43 +1000

    converted most

--- main.py
+++ main.py
@@ -325,6 +325,9 @@ async def on_message(message):
         await message.reply(f'scraped **{sliced}**')
 
 
+# Slash Commands
+
+
 @client.slash_command(guild_ids=[740886739538673664], description="Displays the leaderboard.")
 async def leaderboard(message):
     with open('leaderboard.json', 'r') as f:
@@ -339,6 +342,7 @@ async def leaderboard(message):
         embed = discord.Embed(title="Leaderboard", color=0x3B88C3)
         embed.add_field(name="Names", value=names, inline=False)
         await message.channel.send(embed=embed)
+        await message.respond('Displaying the leaderboard', ephemeral=True)
 
 @client.slash_command(guild_ids=[740886739538673664], description="Starts the game.")
 async def start(message):
@@ -460,6 +464,139 @@ async def start(message):
         embed.add_field(name="Names", value=names, inline=False)
         leaderboard = await lchannel.send(embed=embed)
         leaderboard_id.clear()
-        leaderboard_id.append(leaderboard.id)     
+        leaderboard_id.append(leaderboard.id) 
+        await message.respond('Started the game!', ephemeral=True)    
+
+
+@client.slash_command(guild_ids=[740886739538673664], description="Defines the leaderboard channel.")
+async def set_channel(message):
+    role = discord.utils.find(lambda r: r.name == 'mol', message.guild.roles)
+    if role not in message.author.roles:
+        return await message.respond('invalid perms')
+    leaderboard_channel.clear()
+    leaderboard_channel.append(message.channel.id)
+    await message.respond(f'Set leaderboard channel to {message.channel.name}', ephemeral=True)    
+
+@client.slash_command(guild_ids=[740886739538673664], description="Rolls the generator again.")
+async def next(message):
+    role = discord.utils.find(lambda r: r.name == 'mol', message.guild.roles)
+    if role not in message.author.roles:
+        return await message.reply('invalid perms')
+
+    await message.delete()
+    channel = await client.fetch_channel(channel_id[0])
+    sent_message = await channel.fetch_message(message_id[0])
+    await sent_message.clear_reactions()
+
+    reaction_1 = '⬆️'
+    reaction_2 = '⬇️'  
+
+    print('creating...')
+    create()
+    print('done')
+    with open('./scores.json') as fp:
+        scores = json.load(fp)
+        higher = scores['higher'] 
+        lower = scores['lower']  
+
+    dt = datetime.now()
+    timestamp = int( dt.timestamp() )
+    print( timestamp )
+    time_left = timestamp + 9   
+
+    embed = discord.Embed(title="React with :arrow_up: or :arrow_down:!", description=f"Finished <t:{time_left}:R>!", color=0x3B88C3) #creates embed
+    file = discord.File("out.png", filename="out.png")
+    embed.set_image(url="attachment://out.png")
+    await sent_message.edit(file=file, embed=embed)   
+
+    await sent_message.add_reaction(reaction_1) 
+    await sent_message.add_reaction(reaction_2)   
+
+    time.sleep(5)
+    embed = discord.Embed(title="React with :arrow_up: or :arrow_down:!", description=f"Time's up!", color=0x3B88C3) #creates embed
+    file = discord.File("done.png", filename="done.png")
+    embed.set_image(url="attachment://done.png")
+    await sent_message.edit(file=file, embed=embed)
+
+    updated_message = await channel.fetch_message(sent_message.id)
+    ones = set()
+    twos = set()
+
+    for reaction in updated_message.reactions:
+        async for user in reaction.users():
+                if not user.bot:
+
+                    if reaction.emoji == reaction_1:
+                        if f'{user}: {reaction_2}' not in twos:
+                            ones.add(f'{user}: {reaction.emoji}')
+                            #print(f'added {user} to ones')
+                            #print(user.name)
+
+                            filename = './leaderboard.json'
+                            dictObj = []
+                            
+                            with open(filename) as fp:
+                                dictObj = json.load(fp)
+
+                            #print(dictObj)
+                            if str(user.id) not in dictObj:
+                            #  print('cannot find')
+                                with open(filename,'r+') as file:
+                                        dictObj = json.load(file)
+                                        dictObj[str(user.id)] = 0
+                                        file.seek(0)
+                                        json.dump(dictObj, file, indent = 4)
+
+                            dictObj.update({str(user.id): dictObj[str(user.id)] + higher,})
+
+                            with open(filename, 'w') as json_file:
+                                json.dump(dictObj, json_file, 
+                                                    indent=4,  
+                                                    separators=(',',': '))
+                        
+                    elif reaction.emoji == reaction_2:
+                        if f'{user}: {reaction_1}' not in ones:
+                            twos.add(f'{user}: {reaction.emoji}')
+                            #print(f'added {user} to twos')
+                            #print(user.name)
+                        
+                            filename = './leaderboard.json'
+                            dictObj = []
+                            
+                            with open(filename) as fp:
+                                dictObj = json.load(fp)
+
+                            #print(dictObj)
+                            if str(user.id) not in dictObj:
+                            # print('cannot find')
+                                with open(filename,'r+') as file:
+                                        dictObj = json.load(file)
+                                        dictObj[str(user.id)] = 0
+                                        file.seek(0)
+                                        json.dump(dictObj, file, indent = 4)
+
+                            dictObj.update({str(user.id): dictObj[str(user.id)] + lower,})
+
+                            with open(filename, 'w') as json_file:
+                                json.dump(dictObj, json_file, 
+                                                    indent=4,  
+                                                    separators=(',',': '))
+                                                    
+    lchannel = await client.fetch_channel(leaderboard_channel[0])
+    updated_message = await lchannel.fetch_message(leaderboard_id[0])
+
+    with open('leaderboard.json', 'r') as f:
+        data = json.load(f)
+
+    top_users = {k: v for k, v in sorted(data.items(), key=lambda item: item[1], reverse=True)}
+
+    names = ''
+    for postion, user in enumerate(top_users):
+        names += f'{postion+1} - <@!{user}> with {top_users[user]}\n'
+
+    embed = discord.Embed(title="Leaderboard", color=0x3B88C3)
+    embed.add_field(name="Names", value=names, inline=False)
+    await updated_message.edit(embed=embed) 
+
 
 client.run(os.getenv('TOKEN'))
