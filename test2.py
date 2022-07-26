a29390fc1f2af3db16462a405e9be041e75526af
asptu <84566762+asptu@users.noreply.github.com>
2022-07-26 23:56:09 +1000

    remove test

--- test2.py
+++
@@ -1,6 +0,0 @@
-
-from commands.concatenate import create2, create
-
-im1, im2, im1_name, im2_name, im1_results, im2_results = create()
-
-create2(im1, im2, im1_name, im2_name, im1_results, im2_results)
